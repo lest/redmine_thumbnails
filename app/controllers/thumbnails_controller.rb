7b6720e5ad2d65c00ad01232c030530104667f2b
lest <just.lest@gmail.com>
2009-07-21 16:02:11 +0300

    draft version

---
+++ app/controllers/thumbnails_controller.rb
@@ -0,0 +1,17 @@
+class ThumbnailsController < ApplicationController
+  unloadable
+  
+  def show
+    attach = Attachment.find(params[:id])
+    thumb_width ||= Setting.plugin_redmine_thumbnails["thumb_width"]
+    thumb_height ||= Setting.plugin_redmine_thumbnails["thumb_height"]
+    img = Magick::Image.read("files/" + attach.disk_filename).first
+    tw = thumb_width.to_i
+    th = thumb_height.to_i
+    img = Magick::Image.read("files/" + attach.disk_filename).first
+    thumb = img.resize_to_fit(tw, th)
+    send_data thumb.to_blob, :type => thumb.mime_type, :disposition => 'inline'
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+end
---
+++ init.rb
@@ -0,0 +1,69 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_thumbnails do
+  name 'Redmine Thumbnails plugin'
+  author 'Just Lest'
+  description ''
+  version '0.0.1'
+  
+  settings :default => {
+    'thumb_width' => '400',
+    'thumb_height' => '0'
+  }, :partial => 'settings/thumbnails_settings'
+end
+
+Redmine::WikiFormatting::Macros.register do
+  desc "Thumb macro"
+  macro :thumb do |obj, args|
+    url_prefix = Setting.host_name.split("/")[1] || ""
+    url_prefix = "/" + url_prefix if url_prefix != ""
+    thumb_width = nil
+    thumb_height = nil
+    args.each do |arg|
+      name, value = arg.split("=")
+      name = name.strip
+      if value
+        if "width" == name
+          thumb_width = value
+        elsif "height" == name
+          thumb_height = value
+        end
+      end
+    end
+    thumb_width ||= Setting.plugin_redmine_thumbnails["thumb_width"]
+    thumb_height ||= Setting.plugin_redmine_thumbnails["thumb_height"]
+    filename = args[0]
+    if obj.is_a?(Issue)
+      container = obj
+    elsif obj.is_a?(Journal)
+      container = obj.issue
+    elsif obj.is_a?(WikiContent)
+      container = obj.page
+    end
+    return nil unless container
+    attach = Attachment.find(:first, :conditions => {
+     :container_id => container.id,
+     :container_type => container.class.name,
+     :filename => filename
+    })
+    return nil unless attach
+    
+    thumb_link = url_for :controller => :thumbnails, :action => :show, :id => attach.id
+    attach_link = url_for :controller => 'attachments', :action => 'download',
+      :id => attach.id, :filename => attach.filename
+    
+    img = Magick::Image.read("files/" + attach.disk_filename).first
+    tw = thumb_width.to_i
+    th = thumb_height.to_i
+    if ((tw == 0 || img.columns < tw) && (th == 0 || img.rows < th))
+      html = <<-eos
+<img src="#{attach_link}" />
+eos
+    else
+      html = <<-eos
+<a class="thumb" href="#{attach_link}"><img src="#{thumb_link}" /></a>
+eos
+    end
+    html
+  end
+end
