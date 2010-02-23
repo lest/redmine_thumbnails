f707fc1e5f0952682da535233947b4d62d400a21
lest <just.lest@gmail.com>
2010-02-23 12:04:07 +0200

    add 'copy to clipboard' flash buttons on dom:loaded event

--- lib/thumbnails_issues_hooks.rb
+++ lib/thumbnails_issues_hooks.rb
@@ -4,31 +4,33 @@ class ThumbnailsIssuesHooks < Redmine::Hook::ViewListener
     out += <<JS
 <script type="text/javascript" src="#{root_path}plugin_assets/redmine_thumbnails/javascripts/ZeroClipboard.js"></script>
 <script type="text/javascript">
-  ZeroClipboard.setMoviePath('#{root_path}plugin_assets/redmine_thumbnails/images/ZeroClipboard.swf');
-  var counter = 0;
-  $$('.issue .attachments p').each(function (element) {
-    var filename = element.down().innerHTML;
-    if (!filename.match(/(jpe?g|png|gif)$/i)) {
-      return;
-    }
+  document.observe('dom:loaded', function() {
+    ZeroClipboard.setMoviePath('#{root_path}plugin_assets/redmine_thumbnails/images/ZeroClipboard.swf');
+    var counter = 0;
+    $$('.issue .attachments p').each(function (element) {
+      var filename = element.down().innerHTML;
+      if (!filename.match(/(jpe?g|png|gif)$/i)) {
+        return;
+      }
 
-    var button = document.createElement('img');
-    var button_id = 'thumbnail_clipboard_' + counter
-    button.id = button_id;
-    button.width = '16';
-    button.height = '16';
-    button.src = '#{root_path}images/copy.png';
+      var button = document.createElement('img');
+      var button_id = 'thumbnail_clipboard_' + counter
+      button.id = button_id;
+      button.width = '16';
+      button.height = '16';
+      button.src = '#{root_path}images/copy.png';
 
-    element.appendChild(button);
+      element.appendChild(button);
 
-    var clip = new ZeroClipboard.Client();
-    clip.setText('{{thumb(' + filename + ', issue=#{context[:issue].id})}}');
-    clip.glue(button_id);
-    clip.addEventListener('onComplete', function () {
-      Effect.Pulsate(button);
+      var clip = new ZeroClipboard.Client();
+      clip.setText('{{thumb(' + filename + ', issue=#{context[:issue].id})}}');
+      clip.glue(button_id);
+      clip.addEventListener('onComplete', function () {
+        Effect.Pulsate(button);
+      });
+
+      counter++;
     });
-
-    counter++;
   });
 </script>
 JS
